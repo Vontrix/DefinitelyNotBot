5e1cd2b7a9aaf5d8876ba024338f3ac1b00be1dd
Vontrix <42079399+Vontrix@users.noreply.github.com>
2018-08-04 01:49:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+    static void UpdatePresence()
+    {
+        DiscordRichPresence discordPresence;
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.details = "Currently running on Vontrix | Hacking";
+        discordPresence.largeImageText = "Numbani";
+        discordPresence.smallImageText = "Numbani";
+        Discord_UpdatePresence(&discordPresence);
+    }
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
