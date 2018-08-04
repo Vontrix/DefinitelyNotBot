4895d622025b2eafc36fbec716bcf712becc4860
Vontrix <42079399+Vontrix@users.noreply.github.com>
2018-08-04 02:18:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.user.setActivity('YouTube', { type: 'WATCHING' });
+client.user.setPresence('YouTube', { type: 'WATCHING' });
 
 client.on('ready', () => {
     console.log('I am ready!');
